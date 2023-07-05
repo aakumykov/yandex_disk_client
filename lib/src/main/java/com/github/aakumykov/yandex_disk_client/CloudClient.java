85b092d5ddcdbf0ea24dc0226aa776f6f3b88920
Андрей Кумыков <aakumykov@yandex.ru>
2023-07-05 12:17:47 +0300

    Добавил два метода скачивания файла: синхронный и асинхронный (с RxJava).

--- lib/src/main/java/com/github/aakumykov/yandex_disk_client/CloudClient.java
+++ lib/src/main/java/com/github/aakumykov/yandex_disk_client/CloudClient.java
@@ -3,20 +3,33 @@ package com.github.aakumykov.yandex_disk_client;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.github.aakumykov.okhttp_file_downloader.exceptions.EmptyBodyException;
+
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import io.reactivex.Completable;
 import io.reactivex.Single;
 
 public interface CloudClient<CloudDirType, CloudFileType, OutputItemType, SortingModeType> {
 
-    // Главные методы
+    // ====== Методы получения облачных объектов ======
+
+    /**
+     * Асинхронно запрашивает список элементов в каталоге, на который указывает ссылка на публичный ресурс,
+     * или его подкаталоге remoteDirName, если он не равен null.
+     */
     Single<List<OutputItemType>> getListAsync(@NonNull String resourceKey,
                                               @Nullable String subdirName,
                                               @NonNull SortingModeType sortingMode,
                                               int startOffset,
                                               int limit);
 
+    /**
+     * Синхронно запрашивает список элементов в каталоге, на который указывает ссылка на публичный ресурс,
+     * или его подкаталоге remoteDirName, если он не равен null.
+     */
     List<OutputItemType> getList(@NonNull String resourceKey,
                                  @Nullable String subdirName,
                                  @NonNull SortingModeType sortingMode,
@@ -28,9 +41,15 @@ public interface CloudClient<CloudDirType, CloudFileType, OutputItemType, Sortin
     Single<Boolean> checkItemExists(@NonNull String resourceKey, @NonNull String remoteFilePath);
 
 
+    // ====== Методы скачивания файлов ======
+
+    void downloadFileTo(@NonNull String url, @NonNull File targetFile) throws EmptyBodyException, com.github.aakumykov.okhttp_file_downloader.exceptions.BadResponseException, IOException, BadResponseException;
+//    File downloadFile(@NonNull String url);
+    Completable downloadFileToAsync(@NonNull String url, @NonNull File targetFile);
+//    Single<File> downloadFileAsync(@NonNull String url);
 
 
-    // Вспомогательные методы
+    // ====== Вспомогательные методы ======
 
     /**
      * Преобразует тип сортировки программы, использующей библиотеку библиотеку,
@@ -50,7 +69,9 @@ public interface CloudClient<CloudDirType, CloudFileType, OutputItemType, Sortin
 
     String cloudFileToString(CloudFileType cloudFileType);
 
-    
+
+    // Исключения
+
     abstract class CloudClientException extends Exception {
         public CloudClientException() {
         }
