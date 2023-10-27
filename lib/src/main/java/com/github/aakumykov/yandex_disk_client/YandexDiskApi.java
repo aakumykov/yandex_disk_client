93b44fe4987ed903eb76401806e6a08252495c81
Андрей Кумыков <aakumykov@yandex.ru>
2023-10-27 17:32:55 +0300

    Метод YandexDiskApi.createDirectory(path).

--- lib/src/main/java/com/github/aakumykov/yandex_disk_client/YandexDiskApi.java
+++ lib/src/main/java/com/github/aakumykov/yandex_disk_client/YandexDiskApi.java
@@ -6,13 +6,22 @@ import com.yandex.disk.rest.json.Resource;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.PUT;
 import retrofit2.http.Query;
 
 interface YandexDiskApi {
 
+    // Приватныя ресурсы
+
     @GET("resources")
     Call<Resource> getResourceByPath(@Header("Authorization") String authToken,
                                      @Query("path") String path);
+
+    @PUT("resources")
+    Call<Resource> createDirectory(@Header("Authorization") String authToken,
+                                   @Query("path") String dirNameOrPath);
+
+    // Публичныя ресурсы
 
     @GET("public/resources")
     Call<Resource> getPublicResource(@Query("public_key") String publicKey,
